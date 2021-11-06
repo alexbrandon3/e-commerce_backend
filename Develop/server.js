0f9e1bdcc12fed09ae64086d3875b65c6563e6da
alexbrandon3 <alexlbrandon@gmail.com>
2021-11-06 17:21:55 -0400

    added README.md, video links, and finished routes

---
+++ Develop/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const routes = require("./routes");
+require("dotenv").config();
+// import sequelize connection
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  console.log("ecommerce_db synced.");
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
